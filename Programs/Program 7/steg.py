80be66c02434f945ed23f7dc98b4c6e40fdc8d19
Masonkta <88817850+Masonkta@users.noreply.github.com>
2024-05-16 15:52:34 -0500

    time lock script

--- Programs/Program 7/steg.py
+++ Programs/Program 7/steg.py
@@ -1,191 +1,140 @@
+# Program:  Byte Method Steganography
+# Desc:     Given an image to hide, this program will embed a JPEG image in another JPEG image using the byte method. 
+
+
+# --- USAGE --- 
+""" 
+Command Line:
+python3 steg.py -o [OFFSET] -i [INTERVAL] -w [WRAPPER FILE] -H [FILE TO HIDE] --output [FILE TO OUTPUT RESULT]
+
+Note: Be sure to check that -H file is small enough to be embedded in -w file.
+"""
+
+
+# --- LIBRARIES ---
 import sys
 import argparse
 
-SENTINEL = [0x0, 0xff, 0x0, 0x0, 0xff, 0x0]
 
-def store_byte_mode(offset, interval, wrapper_file, hidden_file):
-    """Store hidden data using byte mode."""
-    with open(wrapper_file, 'rb') as wrapper:
-        wrapper_data = bytearray(wrapper.read())
+# --- CONSTANTS ---
+STORE = None 
+RETRIEVE = None
+WRAPPER = None
+HIDDEN = None
+OFFSET = None 
+INTERVAL = None
+OUTPUT = None
+sentinel = bytes([0x00, 0xFF, 0x00, 0x00, 0xFF, 0x00])
 
-    with open(hidden_file, 'rb') as hidden:
-        hidden_data = bytearray(hidden.read())
 
-    i = 0
-    while i < len(hidden_data):
-        if (offset >= len(wrapper_data)):
-            wrapper_data.append(0x00)
-        else:
-            wrapper_data[offset] = hidden_data[i]
-            i += 1
-    
-    i = 0
-    while i < len(SENTINEL):
-        if offset >= len(wrapper_data):
-            wrapper.append(0x00)
-        else:
-            wrapper_data[offset] = SENTINEL[i]
-            offset += interval
-            i += 1
-    
-    sys.stdout.buffer.write(wrapper_data)
+# --- DEBUG ---
+DEBUG = False
 
-def store_bit_mode(offset, interval, wrapper_file, hidden_file):
-    with open(wrapper_file, 'rb') as wrapper:
-        wrapper_data = bytearray(wrapper.read())
 
-    with open(hidden_file, 'rb') as hidden:
-        hidden_data = bytearray(hidden.read())
+# --- METHODS ---
+def parseArgs():
+    parser = argparse.ArgumentParser()
 
-    i = 0
-    while i < len(hidden_data):
-        for j in range(7):
-            wrapper_data[offset] &= 0b11111110
-            wrapper[offset] |= ((hidden_data[i]& 0b10000000) >> 7)
-            if(hidden_data[i] << 1 > 255):
-                hidden_data[i] = (hidden_data[i]<<1)&(2**8-1)
-            else:
-                hidden_data[i]<<=1
-                
-                offset += interval
-    
-    i = 0
-    while i < len(SENTINEL):
-        for j in range(7):
-            wrapper_data[offset] &= 0b11111110
-            wrapper_data[offset] |= ((SENTINEL[i] & 0b10000000) >> 7)
-            if(SENTINEL[i] << 1 > 255):
-                SENTINEL[i] = (SENTINEL[i]<<1)&(2**8-1)
-            else:
-                SENTINEL[i] <<=1
-            offset += interval
-        i+=1
+    parser.add_argument('-s', '--store', required=False, action='store_true', help="Embeds a hidden JPEG within a wrapper JPEG")
+    parser.add_argument('-r', '--retrieve', required=False, action='store_true', help="Pulls a hidden JPEG out of a wrapper JPEG")
+    parser.add_argument('-o', '--offset', required=True, type=int, help="Offsets <val> number of bytes before encrypting/decrypting")
+    parser.add_argument('-i', '--interval', required=True, type=int, help="Embeds a byte of data at <val> interval")
+    parser.add_argument('-w', '--wrapper', required=True, help="The wrapper file used to hide the JPEG")
+    parser.add_argument('-H', '--hidden', required=False, help="The file to be hidden in the wrapper")
+    parser.add_argument('--output', type=str, required=True, help='Output image file')
 
-    sys.stdout.buffer.write(wrapper)
-
-def retrieve_byte_mode(offset, interval, wrapper_file):
-    """Retrieve hidden data using byte mode."""
-    with open(wrapper_file, 'rb') as wrapper:
-        wrapper_data = bytearray(wrapper.read())
-    
-    extracted_data = bytearray()
-    checkSent = []
-    check = 0
-
-    while offset < len(wrapper_data):
-        b = wrapper_data[offset]
-        if b == SENTINEL[0]:
-            checkSent = []
-            offset2 = offset
-            test = b
-            for byte in SENTINEL:
-                checkSent.append(test)
-                offset2 += interval
-                if offset2 <= len(wrapper_data):
-                    test = wrapper_data[offset2]
-            if checkSent == SENTINEL:
-                offset = offset + interval
-                b = wrapper_data[offset]
-                break
-        extracted_data.append(b)
-        offset += interval
-    
-    sys.stdout.buffer.write(extracted_data)
-
-def retrieve_bit_mode(offset, interval, wrapper_file):
-    with open(wrapper_file, 'rb') as wrapper:
-        wrapper_data = bytearray(wrapper.read())
-
-    hidden = []
-    hidden_sentinel = []
-    sentinel_count = 0
-
-    while(offset < len(wrapper_data)):
-        wrapper_byte = wrapper_data[offset]
-        lsb_shift = 0
-
-        # lsb: least significant bit of current byte
-        for j in range(8):
-            if offset >= len(wrapper_data):
-                break
-
-            # grab lsb
-            lsb = wrapper_data[offset] & 1
-            lsb_shift = lsb_shift ^ (lsb << 7-j)
-
-            offset += interval
-
-        byte = lsb_shift#bytearray(lsb_shift)
-
-        # check if byte matches sentinel
-        if byte == SENTINEL[sentinel_count]:
-            # save byte, update sentinel index
-            hidden_sentinel.append(byte)
-            sentinel_count += 1
-        else:
-            # if hidden sentinel is full, then store values and reset
-            if len(hidden_sentinel) > 0:
-                hidden_sentinel.reverse()
-                for byte_hs in hidden_sentinel:
-                    hidden.append(byte_hs)
-                sentinel_count = 0
-                hidden_sentinel = []
-
-                # save current byte to final file
-            hidden.append(byte)
-
-        if hidden_sentinel == SENTINEL:
-            break
-    sys.stdout.buffer.write(bytearray(hidden))
-
-def main():
-    """Main function to parse command-line arguments and execute appropriate operations."""
-    # Initialize argument parser with description
-    parser = argparse.ArgumentParser(description="Hide or retrieve data from a wrapper file using steganography")
-
-    # Define command-line arguments
-    parser.add_argument("-s", action="store_true", help="Store hidden data")
-    parser.add_argument("-r", action="store_true", help="Retrieve hidden data")
-    parser.add_argument("-b", action="store_true", help="Bit mode")
-    parser.add_argument("-B", action="store_true", help="Byte mode")
-    parser.add_argument("-o", type=int, default=0, help="Offset value (default is 0)")
-    parser.add_argument("-i", type=int, default=1, help="Interval value (default is 1)")
-    parser.add_argument("-w", type=str, required=True, help="Wrapper file path")
-    parser.add_argument("-f", type=str, help="Hidden file path")
-
-    # Parse command-line arguments
     args = parser.parse_args()
+
+    global WRAPPER 
+    WRAPPER = args.wrapper
+    if args.hidden is not None:
+        global HIDDEN 
+        HIDDEN = args.hidden 
+    global OFFSET 
+    OFFSET = args.offset 
+    global INTERVAL 
+    INTERVAL = args.interval 
+    global STORE 
+    STORE = args.store
+    global RETRIEVE 
+    RETRIEVE = args.retrieve
+    global OUTPUT 
+    OUTPUT = args.output
+
+def insertSentinel():
+    with open(HIDDEN, "ab") as appendSent:
+    appendSent.write(SENTINEL)
+
+def embeddingData():
+    with open(HIDDEN, 'rb') as hiddenFile:
+        hiddenData = hiddenFile.read() 
+
+    with open(WRAPPER, 'r+b') as wrapperFile:
+        wrapperData = bytearray(wrapperFile.read())
+
+    index = OFFSET
+
+    for byte in hiddenData:
+        wrapperData[index] = byte
+        index += INTERVAL
     
-    # Check for conflicting options
-    if args.s and args.r:
-        print("Error: Both store (-s) and retrieve (-r) options cannot be specified at the same time.")
-        sys.exit(1)
+    with open(OUTPUT, 'wb') as outputFile:
+        outputFile.write(wrapperData)
 
-    if args.B and args.b:
-        print("Error: Both byte mode (-B) and bit mode (-b) cannot be specified at the same time.")
-        sys.exit(1)
+def dataExtraction():
+    with open(WRAPPER, 'rb') as wrapperFile:
+        wrapperData = wrapperFile.read()
+    index = OFFSET
 
-    # Perform the specified operation
-    if args.s:  # Store hidden data
-        if args.B:  # Byte mode
-            store_byte_mode(args.o, args.i, args.w, args.f)
-        elif args.b:  # Bit mode
-            store_bit_mode(args.o, args.i, args.w, args.f)
-        else:
-            print("Error: Either byte mode (-B) or bit mode (-b) must be specified.")
-            sys.exit(1)
-    elif args.r:  # Retrieve hidden data
-        if args.B:  # Byte mode
-            retrieve_byte_mode(args.o, args.i, args.w)
-        elif args.b:  # Bit mode
-            retrieve_bit_mode(args.o, args.i, args.w)
-        else:
-            print("Error: Either byte mode (-B) or bit mode (-b) must be specified.")
-            sys.exit(1)
-    else:
-        print("Error: Either store (-s) or retrieve (-r) option must be specified.")
-        sys.exit(1)
+    while index < len(wrapperData):
+        byte = wrapperData[index]
+        if not byte:
+            if DEBUG:
+                print("End of file reached.")
+            break 
+        extracted.append(byte)
 
+        if len(extracted) >= 6 and extracted[-6:] == sentinel:
+            if DEBUG:
+                print("Successfully extracted message")
+            break
+
+        index += INTERVAL
+
+    with open(OUTPUT, 'wb') as outputFile:
+        outputFile.write(extracted)
+
+
+# --- DRIVER ---
 if __name__ == "__main__":
-    main()
+    parseArgs()
 
+    if STORE is not None:
+        insertSentinel()
+        embeddingData() 
+    
+    if RETRIEVE is not None:
+        dataExtraction()
+
+
+# --- PERSONAL DOCUMENTATION ---
+# Storage:
+# 1.    parse all the command line arguments for use within the program
+# 2.    append a sentinel value to the end of the hidden file 
+# 3.    embed the data 
+#           - first need to open the hidden file and read all the data to memory 
+#           - next, open the wrapper file in read write mode 
+#           - seek to offset in wrapper file, then write a byte from hidden file to wrapper file at that location 
+#           - go to next interval, and add the next byte from hidden file to wrapper 
+#           - repeat 
+#           - Notes:    Because opening both files in binary mode, seeking will immediately traverse by bytes, not bits.
+#                       Also, when using "with open", it automatically closes the files after the method ends. Lastly, the
+#                       "bytes" method is a built in method that converts the arguments given into a "byte object". 
+#                       Parameter options include decimal numbers, hex values, binary values, etc. 
+
+# Extraction:
+# 1.    parse all command line arguments 
+# 2.    extract the data 
+#           - open wrapper file in read write binary mode 
+#           - seek to offset, copy data at that location and append to a byte array 
+#           - continuously seek to intervals, copying and appending data until end of file (wrapper)  
